aeecd5a5c89ea4a8647cc59bb7fb9bd522f8cf87
ayushpareek2003 <ayushpareek1980@gmail.com>
2025-03-17 03:22:49 +0530

    added conditions and for loops

--- inc/lexer.h
+++ inc/lexer.h
@@ -8,15 +8,26 @@
 
 enum Token {
     tok_eof = -1,
+  
+    
     tok_def = -2,
     tok_extern = -3,
+
     tok_identifier = -4,
-    tok_number = -5
-};
+    tok_number = -5,
+  
+ 
+    tok_if = -6,
+    tok_then = -7,
+    tok_else = -8,
+    tok_for = -9,
+    tok_in = -10
+  };
+  
 
 extern std::string IdentifierStr;
 extern double NumVal;
 
 int gettok();
 
-#endif // LEXER_H
+#endif
